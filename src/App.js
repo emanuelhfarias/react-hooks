199af1f9035a19c6c4da966eca05fe4036bc20c8
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-03-22 17:47:02 -0300

    demonstrate useEffect
    substitute componentDidMount, componentDidUpdate and componentWillMount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, setTechs] = useState(['ReactJS', 'React Native']);
+  const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTechs([...techs, newTech]);
     setNewTech('');
   }
+
+  // call one time when component mount (componentDidMount)
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  // call every time techs updates (componentDidUpdate)
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
